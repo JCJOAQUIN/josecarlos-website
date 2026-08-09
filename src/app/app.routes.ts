f5034015b446a70ad1333cbc6197592b1235db66
José Carlos Joaquín Vazquez <josecarlos19882546@gmail.com>
2026-08-09 02:22:42 -0400

    refactor(app): establish application architecture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Shell } from './layout/shell/shell';
+import { HomePage } from './features/home/pages/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Shell,
+    children: [
+        {
+            path: '',
+            component: HomePage,
+        },
+    ],
+  },
+];
